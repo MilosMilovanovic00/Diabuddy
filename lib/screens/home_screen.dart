a9924d0314206f5af17e040da5d04fc6925b0908
MilosMilovanovic00 <102919389+MilosMilovanovic00@users.noreply.github.com>
2023-04-15 20:25:22 +0200

    Project Initialization

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/theme/theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+var applicationTheme = ThemeData(
+  textTheme: const TextTheme(
+    titleLarge: TextStyle(
+      fontSize: 20,
+      fontStyle: FontStyle.italic,
+    ),
+  ),
+);
